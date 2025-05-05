4be2adb35c75b16225ec54564dd93e577d743d14
andreamaluta <andreamaluta@gmail.com>
2025-05-05 15:19:17 +0200

    [UPD] made the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+
+const Header = () => {
+    return (
+        <header className="container">
+            <figure>
+                <img src="dc-logo.png" alt="" />
+            </figure>
+
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
