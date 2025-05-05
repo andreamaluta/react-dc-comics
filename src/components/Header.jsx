d1498e13a5231e35a81c5cb2c3802a87450d9a2e
andreamaluta <andreamaluta@gmail.com>
2025-05-05 15:25:50 +0200

    [FIX] fixed the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,29 @@
 
 const Header = () => {
     return (
-        <header className="container">
-            <figure>
-                <img src="dc-logo.png" alt="" />
-            </figure>
+        <header>
+            <div className="container">
+                <div className="header-style">
+                    <figure>
+                        <img src="dc-logo.png" alt="" />
+                    </figure>
 
-            <ul>
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBLES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
-            </ul>
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
         </header>
     )
 }
