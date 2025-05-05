f012f4654f6af18944093d91e8588ffc5c23af6b
andreamaluta <andreamaluta@gmail.com>
2025-05-05 15:39:30 +0200

    [UPD] made the top main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+      <Main />
+    </>
+
   )
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+const Main = () => {
+    return (
+        <main>
+            <div className="main-top">
+                <div className="container">
+                    <h2>&#8594; Content goes here &#8592;</h2>
+                </div>
+            </div>
+
+
+        </main>
+    )
+}
+
+export default Main
