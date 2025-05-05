2d42c2fb468836964c3d018ab36eeee65e0dfcb6
andreamaluta <andreamaluta@gmail.com>
2025-05-05 16:01:42 +0200

    [UPD] finish the main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,32 @@ const Main = () => {
                     <h2>&#8594; Content goes here &#8592;</h2>
                 </div>
             </div>
-
+            <div className="main-bottom">
+                <div className="container">
+                    <ul>
+                        <li>
+                            <img src="src/assets/img/buy-comics-digital-comics.png" alt="" />
+                            DIGITAL COMICS
+                        </li>
+                        <li>
+                            <img src="src/assets/buy-comics-merchandise.png" alt="" />
+                            DC MERCHANDISE
+                        </li>
+                        <li>
+                            <img src="src/assets/img/buy-comics-subscriptions.png" alt="" />
+                            SUBSCRIPTION
+                        </li>
+                        <li>
+                            <img src="src/assets/img/buy-comics-shop-locator.png" alt="" />
+                            COMIC SHOP LOCATOR
+                        </li>
+                        <li>
+                            <img src="src/assets/img/buy-dc-power-visa.svg" alt="" />
+                            DC POWER VISA
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
         </main>
     )
