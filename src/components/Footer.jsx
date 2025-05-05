3c963f313aa75482a9dae07477fcb3393ad62755
andreamaluta <andreamaluta@gmail.com>
2025-05-05 18:35:42 +0200

    [UPD] made the top footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,51 +3,57 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-top">
-                <div className="container foot-list">
-                    <div>
-                        <h3>DC COMICS</h3>
-                        <ul>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                        </ul>
-                        <h3>SHOP</h3>
-                        <ul>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                        </ul>
+                <div className="container d-flex">
+                    <div className="foot-list">
+                        <div>
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li>Character</li>
+                                <li>Movies</li>
+                                <li>Comics</li>
+                                <li>Tv</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectables</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>DC</h3>
+                            <ul>
+                                <li>Terms of use</li>
+                                <li>Privacy Policy</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop help</li>
+                                <li>Contact us</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>SITES</h3>
+                            <ul>
+                                <li>DC</li>
+                                <li>Mad MAgazine</li>
+                                <li>DC kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div>
-                        <h3>DC</h3>
-                        <ul>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>SITES</h3>
-                        <ul>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                            <li>lorem ipsum</li>
-                        </ul>
-                    </div>
+                    <figure>
+                        <img src="src/assets/img/dc-logo-bg.png" alt="" />
+                    </figure>
                 </div>
+
             </div>
             <div className="footer-bottom"></div>
         </footer>
