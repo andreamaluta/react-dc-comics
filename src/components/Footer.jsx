7392649143510a6e0d5389e3899fbe9d32da3ad4
andreamaluta <andreamaluta@gmail.com>
2025-05-05 16:25:19 +0200

    [UPD] added the lists to top footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-top">
+                <div className="container foot-list">
+                    <div>
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                        </ul>
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>DC</h3>
+                        <ul>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                            <li>lorem ipsum</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom"></div>
+        </footer>
+    )
+}
+
+export default Footer
