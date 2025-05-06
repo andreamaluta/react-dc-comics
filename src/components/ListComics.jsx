1dce8483c1f0539e47bda609e2c5afc37e21acd1
andreamaluta <andreamaluta@gmail.com>
2025-05-06 15:43:25 +0200

    [UPD] made the firsts cards with props

--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -7,8 +7,8 @@ const ListComics = () => {
         <div>
             <div className="container card-container">
 
-                <ComicCard />
-                <ComicCard />
+                <ComicCard src="https://imgs.search.brave.com/OU2E3_3vA7flyv-SJ3TcFW1d99KDlp5ZfmW9ylLLFDo/rs:fit:1012:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3LnN1/cGVycG91dm9pci5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMDcvQW1lcmlj/YW4tVmFtcGlyZS0x/OTc2LTAxLWNvdi1B/bGJ1cXVlcnF1ZS0y/MDIwLmpwZz9yZXNp/emU9MTAxMiUyQzE1/NTYmc3NsPTE" title="Action Comics" />
+                <ComicCard src="https://imgs.search.brave.com/OU2E3_3vA7flyv-SJ3TcFW1d99KDlp5ZfmW9ylLLFDo/rs:fit:1012:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3LnN1/cGVycG91dm9pci5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMDcvQW1lcmlj/YW4tVmFtcGlyZS0x/OTc2LTAxLWNvdi1B/bGJ1cXVlcnF1ZS0y/MDIwLmpwZz9yZXNp/emU9MTAxMiUyQzE1/NTYmc3NsPTE" title="American Vampire 1976" />
                 <ComicCard />
                 <ComicCard />
                 <ComicCard />
